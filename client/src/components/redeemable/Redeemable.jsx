3a5437173f7585b58c1d47a28a2485c0819f6cad
nuttdguy <pygnasak@gmail.com>
2020-03-13 16:01:11 -0700

    Layout the intial html structure of redeemable component

--- client/src/components/redeemable/Redeemable.jsx
+++ client/src/components/redeemable/Redeemable.jsx
@@ -13,7 +13,35 @@ class Redeemable extends Component {
         
         return (
             <div>
-                Redeemables
+                <ul>
+                    <li>Embers</li>
+                    <li>Sparks</li>
+                </ul>
+
+                <ul>
+                    <li>Skills appear here on trance_musics channel</li>
+                </ul>
+        
+                <ul>
+                    <li>redeemable-1</li>
+                    <li>redeemable-2</li>
+                    <li>redeemable-3</li>
+                </ul>
+                <ul>
+                    <li>redeemable-4</li>
+                    <li>redeemable-5</li>
+                    <li>redeemable-6</li>
+                </ul>
+                <ul>
+                    <li>redeemable-7</li>
+                    <li>redeemable-8</li>
+                    <li>redeemable-9</li>
+                </ul>
+                <ul>
+                    <li>lvl 34</li>
+                    <li>sparks 69,000</li>
+                    <li>gems 0 <a>+</a></li>
+                </ul>
             </div>
         )
     }
