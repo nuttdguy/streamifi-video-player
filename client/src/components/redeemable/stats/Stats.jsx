3ef67b2f657eacaec9bb602bb346346c7b0c79af
nuttdguy <pygnasak@gmail.com>
2020-03-19 14:56:03 -0700

    Correct stats bar from shifting when redeemable img is clicked

--- client/src/components/redeemable/stats/Stats.jsx
+++ client/src/components/redeemable/stats/Stats.jsx
@@ -33,14 +33,16 @@ class Stats extends Component {
         const { onShowShop, walletStats } = this.props;
 
         return (
-            <div className={styles.stats}>
-                <div className={styles.lvl}>
-                    <div className={styles.lvlContainer} >
+            <div className={styles.statsContainer}>
+
+                <div className={styles.levelContainer}>
+
+                    <div className={styles.level} >
                         <span>LVL {walletStats.level}</span>
                     </div>
                 </div>
 
-                <div>
+                <div className={styles.balanceContainer}> 
 
                     <div className={styles.sparkContainer}>
                         <img src={walletStats.tokens.sparks} />
@@ -50,7 +52,9 @@ class Stats extends Component {
                     <div className={styles.emberContainer}>
                         <img src={walletStats.tokens.embers} />
                         <span>{walletStats.embers}</span>
-                        <button onClick={onShowShop}>+</button>
+                        <button onClick={onShowShop}>
+                            <span>+</span>
+                        </button>
                     </div>
                 </div>
 
